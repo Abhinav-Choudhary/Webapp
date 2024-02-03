ce2b76ba9df4f2269e33564bd0c781ace20432aa
Abhinav Choudhary <choudhary.ab@northeastern.edu>
2024-02-03 17:39:23 -0500

    Pushing local code to upstream alongside updated gitignore

---
+++ src/main/java/edu/abhinav/cloud/service/UserService.java
@@ -0,0 +1,21 @@
+package edu.abhinav.cloud.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import edu.abhinav.cloud.pojo.User;
+import edu.abhinav.cloud.repository.UserRepository;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+    
+}
