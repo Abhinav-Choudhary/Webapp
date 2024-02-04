27a9fc08d3725fd47c44574e31bb0cd8bcb2bc41
Abhinav Choudhary <choudhary.ab@northeastern.edu>
2024-02-04 13:42:52 -0500

    added assignment 1 code and workflow file

---
+++ src/test/java/edu/abhinav/cloud/CloudApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.abhinav.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
