f7f2a536090c1a1648e8ccc900fface71e35bddc
Abhinav Choudhary <choudhary.ab@northeastern.edu>
2024-02-27 22:43:54 -0500

    updated scripts, and service for cloudsql

--- scripts/enableServices.sh
+++ scripts/enableServices.sh
@@ -3,8 +3,5 @@
 echo "---------------------------------------Running daemon reload----------------------------------------------"
 sudo systemctl daemon-reload
 
-echo "---------------------------------------Setting mysqld to run by default-----------------------------------"
-sudo systemctl enable mysqld.service
-
 echo "--------------------------------------Setting csye6225.service to run by default--------------------------"
 sudo systemctl enable csye6225.service
